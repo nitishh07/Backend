9c4b1c13033d1cecccc317697ba200a85ab2033f
Nitish Kumar <nitishkumar@kiit.ac.in>
2025-03-23 12:12:26 +0530

    Initial Commit

--- REV/index.js
+++ REV/index.js
@@ -1,16 +1,44 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+
+//Server Listening
 let port = 8080;
+
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
+
+// app.get("/",(req,res)=>{
+//     res.render("home.ejs");
+// });
 
 app.listen(port, ()=>{
     console.log(`app is listening on port ${port}`);
 });
 
-app.use((req, res) =>{
-    console.log("request received");
-    res.send({
-        name : "apple",
-        color: "red",
-    });
+// //Request Recieved
+// app.use((req, res) =>{
+//     console.log("request received");
+
+//     //Response Send
+//     res.send({                      
+//         name : "apple",
+//         color: "red",
+//     });
+// });
+
+// app.get("/apple",(req,res)=>{
+//     res.send("apple is the path");
+// });
+
+// app.get("/:username",(req,res)=>{
+//     res.send(`Hello ${req.params.username}`);
+// });
+
+app.get("/ig/:username",(req,res)=>{
+    const followers = ["adam","bob","charlie"];
+    let {username} = req.params;
+    res.render("instagram.ejs",{username , followers});
 });
